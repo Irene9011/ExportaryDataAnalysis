dc5e3676953fb9969c6cbb9eeebb5d2058c95d0f
Irene9011 <irene1990.xu@gmail.com>
2015-03-22 12:08:12 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+#read the file
+NEI <-readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#get the subset data
+pm25_annual <- tapply(NEI$Emissions, NEI$year, sum)
+
+#plot the graph
+png("plot1.png")
+plot( names(pm25_annual), pm25_annual, type ="l", xlab="year", ylab="PM2.5 emissions", main="PM2.5 emissions by year", col="red")
+ 
+dev.off()

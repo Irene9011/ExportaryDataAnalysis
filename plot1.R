b4dbaf5a9cb5db413ef71ba037413018cf11deb4
Irene9011 <irene1990.xu@gmail.com>
2015-03-22 13:01:04 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,6 +7,6 @@ pm25_annual <- tapply(NEI$Emissions, NEI$year, sum)
 
 #plot the graph
 png("plot1.png")
-plot( names(pm25_annual), pm25_annual, type ="l", xlab="year", ylab="PM2.5 emissions", main="PM2.5 emissions by year", col="red")
+plot( names(pm25_annual), pm25_annual, type ="l", xlab="year", ylab="PM2.5 emissions", xlim= c(1999,2008), main="PM2.5 emissions by year", col="red")
  
 dev.off()
